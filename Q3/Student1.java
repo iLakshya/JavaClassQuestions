341ad5efb1873b9573f9f2d7a5ad20f000858a3e
Lakshya Goel <lgoel340@gmail.com>
2022-11-17 03:29:16 +0530

    Q3

---
+++ Q3/Student1.java
@@ -0,0 +1,16 @@
+package Q3;
+public class Student1
+{
+    String firstName, secondName;
+    int age;
+    public static void main(String args[])
+    {
+        Student1 obj = new Student1();
+        obj.firstName = "Lakshya";
+        obj.secondName = "Goel";
+        obj.age = 20;
+        System.out.println(obj.firstName);
+        System.out.println(obj.secondName);
+        System.out.println(obj.age);
+    }    
+}
